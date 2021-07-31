1373394100c6d62750166ab6d7131aad9111bd44
Lilian Zhou <lilianwzhou@berkeley.edu>
2021-07-30 20:38:06 -0700

    [backend] added JWT auth

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./login/Login";
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
